314a1e49b4fa980b65c07b7f4845d7e2e8059738
noshacode <nuhasaleh2010@gmail.com>
2022-03-07 15:12:44 +0100

    routes and links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import CreateQuestion from './components/CreateQuestion';
+import Layout from './components/Layout';
+import Logout from './components/Logout';
+import NotFound from './components/NotFound';
 import QuestionsList from './components/QuestionsList';
+import ReadQuestion from './components/ReadQuestion';
+import Signup from './components/Signup';
 
 function App() {
 
   return (
-    <div className="App">
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<QuestionsList />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="logout" element={<Logout />} />
+        <Route path="questions">
+          <Route index element={<QuestionsList />} />
+          <Route path=":id" element={<ReadQuestion />} />
+          <Route path="new" element={<CreateQuestion />} />
+        </Route>
       
-      <QuestionsList />
-    </div>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/components/ReadQuestion.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ReadQuestion() {
+  return (
+    <div>ReadQuestion</div>
+  )
+}
